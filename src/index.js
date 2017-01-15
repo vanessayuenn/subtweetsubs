f7eb04e3c1cb8a46a49a644208bcf8eb7fa0b756
Varun Vachhar <varunvachhar@gmail.com>
2017-01-15 14:24:27 -0500

    post a reply tweet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var app = express();
+import express from 'express';
+import { initStream } from './tweets';
 
-import './tweets';
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello 🌭!')
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
+
+initStream();
--- src/tweets.js
+++ src/tweets.js
@@ -1,8 +1,8 @@
-import Twit from 'twit';
 import R from 'ramda';
 import fs from 'fs';
-import SUBS from './subs';
+import Twit from 'twit';
 import pickImage from './img-picker';
+import SUBS from './subs';
 
 const twit = new Twit({
   consumer_key:         '66L1RPtLQKyGHED7sJDmDtrXE',
@@ -11,25 +11,76 @@ const twit = new Twit({
   access_token_secret:  'pELot1gEmuXdySc4Wr5a7rJm26Gqz1R6N9PquxuO5gn0h',
 });
 
-const respondToMentions = R.compose(
-  R.when(
-    R.test(/^@subtweetsubs.+/),
-    R.tap(console.log),
-  ),
-  R.prop('text'),
-);
+const emojis = ['👊', '🔥', '👍', '🎉', '💁', '🙃', '🍕', '😎', '😘', '👏',
+  '✌️', '👌', '👈', '👙', '🐷', '🍟'];
 
-const stream = twit.stream('user');
-stream.on('tweet', respondToMentions);
+function handleResponse(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+}
 
-function checkFileExists(filePath) {
-  return new Promise((resolve, reject) => {
-    return fs.access(
-      filePath,
-      fs.constants.R_OK | fs.constants.W_OK,
-      error => error ? reject(error) : resolve(filePath)
+function getImage(text) {
+  const img = pickImage(SUBS, text);
+  return fs.readFileSync(img.filePath, { encoding: 'base64' });
+}
+
+function composeMessage(usernames) {
+  const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+  return `${ usernames } ${ emoji }`;
+};
+
+function composeStatus(id, usernames, mediaIdStr) {
+  return {
+    in_reply_to_status_id: id,
+    status: composeMessage(usernames),
+    media_ids: [mediaIdStr],
+  };
+}
+
+function postStatusWithMedia(metaParams, id, usernames, mediaIdStr) {
+  if (!usernames.includes('subtweetsubs')) {
+    twit.post('media/metadata/create', metaParams, (err, data) => {
+      if (!err) {
+        const statusObj = composeStatus(id, usernames, metaParams.media_id);
+        twit.post('statuses/update', statusObj, handleResponse);
+      }
+    });
+  }
+}
+
+function postAReply(tweet) {
+  const mediaObj = { media_data: getImage(tweet.text) };
+
+  twit.post('media/upload', mediaObj, (err, data) => {
+    const mediaIdStr = data.media_id_string
+    const altText = 'a sub sandwich';
+    const metaParams = { media_id: mediaIdStr, alt_text: { text: altText } };
+    postStatusWithMedia(
+      metaParams,
+      tweet.id_str,
+      `@${tweet.in_reply_to_screen_name} @${tweet.user.screen_name}`,
     );
   });
 }
 
-const img = pickImage(SUBS, 'basic');
+const respondToMentions = R.compose(
+  R.when(
+    R.compose(
+      R.test(/@subtweetsubs/),
+      R.tap(console.log),
+      R.prop('text'),
+    ),
+    R.compose(
+      postAReply,
+      R.tap(() => console.log('respondToMentions')),
+    ),
+  ),
+);
+
+export function initStream() {
+  const stream = twit.stream('user');
+  stream.on('tweet', respondToMentions);
+}
