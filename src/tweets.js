9be6fe5c13c592e47b289b0372d4581b9d5c9d41
Varun Vachhar <varunvachhar@gmail.com>
2017-01-15 14:59:54 -0500

    real config

--- src/tweets.js
+++ src/tweets.js
@@ -3,13 +3,9 @@ import fs from 'fs';
 import Twit from 'twit';
 import pickImage from './img-picker';
 import SUBS from './subs';
+import config from './config';
 
-const twit = new Twit({
-  consumer_key:         '66L1RPtLQKyGHED7sJDmDtrXE',
-  consumer_secret:      'FpUpO3DAI1iS6h0y3bUNnfzxqpIMZsT9kNP6E5SZjGnv7tlF9J',
-  access_token:         '820443039151886336-CEL5b1NQdfwbkwjdXIF2PEUbIydux27',
-  access_token_secret:  'pELot1gEmuXdySc4Wr5a7rJm26Gqz1R6N9PquxuO5gn0h',
-});
+const twit = new Twit(config);
 
 const emojis = ['👊', '🔥', '👍', '🎉', '💁', '🙃', '🍕', '😎', '😘', '👏',
   '✌️', '👌', '👈', '👙', '🐷', '🍟'];
